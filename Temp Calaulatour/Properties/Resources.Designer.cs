247d9260341e91cccde4948cb6d10caec01d1fbe
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-09-05 21:44:57 -0600

    added image of app

--- Temp Calaulatour/Properties/Resources.Designer.cs
+++ Temp Calaulatour/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Temp_Calaulatour.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap app {
+            get {
+                object obj = ResourceManager.GetObject("app", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap background {
             get {
                 object obj = ResourceManager.GetObject("background", resourceCulture);
